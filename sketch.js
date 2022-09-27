d70570fc05cd64d457c82799772b732837819119
DaviRava <104589408+DaviRava@users.noreply.github.com>
2022-09-27 14:48:26 -0300

    basquete 1.4
    mecanica

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ const Body = Matter.Body;
 var jogador, jogadorIMG, jogadorA;
 var ball, ballIMG;
 var backgroundIMG;
+var protecao1, protecao2, protecao3, ponto
+var placar = 0
 
 
 
@@ -20,23 +22,28 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1200,600);
+  createCanvas(1200, 600);
 
   engine = Engine.create();
   world = engine.world;
 
   jogadorIMG.frameDelay = 40
-  jogador = createSprite(width/2 + 100, height - 120)
+  jogador = createSprite(width / 2 + 100, height - 120)
   jogador.addImage("jogadorIMG", jogadorIMG)
   jogador.addAnimation("jogadorA", jogadorA)
   jogador.scale = 0.25
 
-  ball = createSprite(width/2 + 126, 180)
+  ball = createSprite(width / 2 + 126, 420)
   ball.addImage("ball", ballIMG)
   ball.scale = 0.006
   console.log(ball.y)
 
-  
+  protecao1 = createSprite(972, 288, 5, 50)
+  protecao3 = createSprite(1022, 288, 5, 50)
+  protecao2 = createSprite(997, 313, 55, 5)
+  ponto = createSprite(997, 307.5, 42.5, 5)
+
+
 }
 
 
@@ -44,31 +51,28 @@ function draw() {
   image(backgroundIMG, 0, 0, 1200, 600)
   Engine.update(engine);
 
-  if(keyIsDown (UP_ARROW)){
-    ball.y -= 1
+  if (keyIsDown(UP_ARROW)) {
+    ball.y -= 10
     console.log(ball.y)
   }
-  
+  if (keyIsDown(DOWN_ARROW)) {
+    ball.y += 10
+    console.log(ball.y)
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+    ball.x -= 10
+    console.log(ball.y)
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    ball.x += 10
+    console.log(ball.y)
+  }
   drawSprites()
-}
 
-function keyPressed(){
-  if(keyCode === UP_ARROW){
-    ball.y -= 1
-    console.log(ball.y)
+  if (ball.isTouching(ponto)) {
+    placar += 3
   }
 
-  if(keyCode === DOWN_ARROW){
-    ball.y += 1
-  }
-
-  if(keyCode === LEFT_ARROW){
-    ball.x -= 1
-  }
-
-  if(keyCode === RIGHT_ARROW){
-    ball.x += 1
-  }
 }
 
 /*velocidade vai aumentar e diminuir com as teclas pra cima e para baixo e o espaco tecla para atirar a bola
